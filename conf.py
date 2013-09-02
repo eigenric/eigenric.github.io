2c40b540054f5ac0de8f59410fb0f8b4d99e7848
Ricardo Ruiz <riki319@gmail.com>
2013-09-02 16:33:10 +0200

    My first commit

---
+++ conf.py
@@ -0,0 +1,38 @@
+THEME = u'snakepy/' 
+
+AUTHOR = u'Ricardo Ruiz'
+SITENAME = u"RicarDotPy"
+SITEURL = 'http://ricardotpy.zz.mu'
+TIMEZONE = "Europe/Madrid"
+
+#Navigation sections and relative URL:
+
+DEFAULT_CATEGORY = 'Uncategorized'
+DATE_FORMAT = {
+'es': '%a %d de %b de %Y'
+}
+DEFAULT_DATE_FORMAT = '%d %m %Y'
+DEFAULT_DATE = 'fs'
+
+DISQUS_SITENAME = "ricardotpy"
+TWITTER_USERNAME = 'riki319'
+GITHUB_URL = 'http://github.com/riki319'
+
+EMAIL = 'riki319@gmail.com'
+SOCIAL = (('github', 'http://github.com/riki319'),
+		  ('twitter', 'http://twitter.com/riki319'),
+		  ('facebook', 'http://facebook.com/riki319'))
+
+BLOGROLL = (('Geekia', 'http://geekia.es'),)
+
+PDF_GENERATOR = False
+REVERSE_CATEGORY_ORDER = True
+LOCALE = ""
+DEFAULT_PAGINATION = 10
+
+FEED_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+
+GOOGLE_ANALYTICS = 'UA-16479483-10'
+STATIC_PATHS = ["images"]
+
---
+++ content/python/empezando-con-pelican.rst
@@ -0,0 +1,7 @@
+Volando con pelican
+#####################
+
+:slug: flying-with-pelican
+:tags: python, pelican, static, blog
+:summary: Los sitios estáticos han muerto. ¡Vivan los sitios estáticos!
+
